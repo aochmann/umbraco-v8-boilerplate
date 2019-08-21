632a8d22079bf8a31ea87ff1a02897c0729a7467
Adrian Ochmann <adrian.ochmann@outlook.com>
2019-08-21 22:20:41 +0200

    Added owin startup to umbraco web project

---
+++ src/MyUmbraco.Web/Startup.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Owin;
+using MyUmbraco.Web;
+using Owin;
+using Umbraco.Web;
+
+
+[assembly: OwinStartup(typeof(Startup))]
+namespace MyUmbraco.Web
+{
+    public class Startup : UmbracoDefaultOwinStartup
+    {
+        public override void Configuration(IAppBuilder app)
+        {
+            base.Configuration(app);
+            // todo: Add configuration
+        }
+    }
+}
