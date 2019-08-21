fc7cf04d07119ebd45c0d9945c1585bc914b1490
Adrian Ochmann <adrian.ochmann@outlook.com>
2019-08-21 23:15:42 +0200

    Prepare startup

--- src/MyUmbraco.Web/App_Start/Startup.cs
+++ src/MyUmbraco.Web/App_Start/Startup.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.Owin;
+﻿using System.Configuration;
+using Microsoft.Owin;
 using MyUmbraco.Web;
 using Owin;
+using Umbraco.Core.Services;
 using Umbraco.Web;
 
 [assembly: OwinStartup("CustomOwinStartup", typeof(Startup))]
@@ -12,6 +14,18 @@ namespace MyUmbraco.Web
         {
             base.Configuration(app);
             // todo: Add configuration
+        }
+
+        protected override void ConfigureServices(IAppBuilder app, ServiceContext services)
+        {
+            base.ConfigureServices(app, services);
+            // todo: DI modules registration
+        }
+
+        protected override void ConfigureMiddleware(IAppBuilder app)
+        {
+            base.ConfigureMiddleware(app);
+            // todo: runtime
         }
     }
 }
